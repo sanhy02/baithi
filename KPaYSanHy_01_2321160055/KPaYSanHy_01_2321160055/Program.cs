1880c60835edb215ef738f4fb5ab1b01b1b1f64c
sanhy02 <155121945+sanhy02@users.noreply.github.com>
2024-02-21 11:13:57 +0700

    Add files via upload

---
+++ KPaYSanHy_01_2321160055/KPaYSanHy_01_2321160055/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPaYSanHy_01_2321160055
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuanLyNhanVien qlnv = new QuanLyNhanVien();
+            int choice;
+            do
+            {
+                Console.WriteLine("====== MENU ======");
+                Console.WriteLine("1.Nhap danh sach nhan vien");
+                Console.WriteLine("2.Hien thi thong tin cac nhan vien");
+                Console.WriteLine("3.Thong ke tong tien luong cong ty");
+                Console.WriteLine("4.Tinh tien luong trung binh cua cac nhan vien");
+                Console.WriteLine("0.Thoat");
+                Console.WriteLine("Nhap lua chon cua ban: ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        qlnv.NhapDS();
+                        break;
+                    case 2:
+                        qlnv.XuatDS();
+                        break;
+                    case 3:
+                        Console.WriteLine($" -Tong tien luong cong ty phai tra: {qlnv.TinhLuongTrungBinh()}");
+                        break;
+                    case 4:
+                        Console.WriteLine($" -Tien luong trung binh cua cac nhan vien: {qlnv.TinhLuongTrungBinh()}");
+                        break;
+                    case 0:
+                        Console.WriteLine(" -Thoat chuong trinh");
+                        break;
+                    default:
+                        Console.WriteLine(" -Lua chon khong hop le");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
---
+++ KPaYSanHy_01_2321160055/KPaYSanHy_01_2321160055/QLNV.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPaYSanHy_01_2321160055
+{
+
+    class NhanVien
+    {
+        public string maso { get; set; }
+        public string hoten { get; set; }
+        public int luongcung { get; set; }
+        public virtual void Nhap()
+        {
+            Console.Write(" -Nhap ma so:");
+            maso = Console.ReadLine();
+            Console.Write(" -Nhap ho ten:");
+            hoten = Console.ReadLine();
+            Console.Write(" -Nhap luong cung:");
+            luongcung = int.Parse(Console.ReadLine());
+        }  
+        public virtual int TinhLuong()
+        {
+            return luongcung;
+        }
+    }
+
+    class NhanVienBienChe : NhanVien
+    {
+        public string mucxeploai { get; set; }
+        public override void Nhap()
+        {
+            base.Nhap();
+            Console.Write(" -Nhap muc xep loai (A, B, C): ");
+            mucxeploai = Console.ReadLine();
+        }
+        public override int TinhLuong()
+        {
+            int thuong = 0;
+            switch (mucxeploai)
+            {
+                case "A": thuong = (int)(1.8 * luongcung);
+                    break;
+                case "B":
+                    thuong = (int)(1.2 * luongcung);
+                    break;
+                case "C":
+                    thuong = (int)(0.8 * luongcung);
+                    break;
+            }
+            return luongcung + thuong;
+        }
+    }
+
+    class NhanVienHopDong : NhanVien
+    {
+        public int doanhthu { get; set; }
+        public override void Nhap()
+        {
+            base.Nhap();
+            Console.Write(" -Nhap doanh thu:");
+            doanhthu = int.Parse(Console.ReadLine());
+        }
+        public override int TinhLuong()
+        {
+            return luongcung + (int)(0.05 * doanhthu);
+        }
+    }
+
+    class QuanLyNhanVien
+    {
+        public List<NhanVien> dsNV { get; set; }
+        public QuanLyNhanVien()
+        {
+            dsNV = new List<NhanVien>();
+        }
+        public void NhapDS()
+        {
+            Console.Write(" -Nhap so luong nhan vien:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i=0;i<n;i++)
+            {
+                Console.WriteLine($" -Nhap thong tin nhan vien thu {i + 1}:");
+                Console.Write(" -Chon loai nhan vien (1 - Bien che, 2 - Hop dong): ");
+                int choice = int.Parse(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1: NhanVienBienChe nvbc = new NhanVienBienChe();
+                        nvbc.Nhap();
+                        dsNV.Add(nvbc);
+                        break;
+                    case 2:NhanVienHopDong nvhd = new NhanVienHopDong();                      
+                        nvhd.Nhap();
+                        dsNV.Add(nvhd);
+                        break;
+                    default:
+                        Console.WriteLine(" -Lua chon khong hop le:");
+                        break;
+                }
+            }while (Console.ReadKey().Key != ConsoleKey.Enter);
+        }
+        public void XuatDS()
+        {
+            foreach (var nv in dsNV)
+            {
+                Console.WriteLine($" -Ma so: {nv.maso},  -Ho ten: {nv.hoten},  -Luong thuc lanh: {nv.TinhLuong()}");
+            }
+        }
+        public int TinhTongLuong()
+        {
+            int tongluong = 0;
+            foreach(var nv in dsNV)
+            {
+                tongluong += nv.TinhLuong();
+            }
+            return tongluong;
+        }
+        public double TinhLuongTrungBinh()
+        {
+            if (dsNV.Count == 0)
+                return 0;
+            double tongluong = TinhTongLuong();
+            return tongluong / dsNV.Count;
+        }
+    }  
+}
